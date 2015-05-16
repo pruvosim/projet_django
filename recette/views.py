ae44a04fed0e812b9ea193f7e1f897c38ef96477
kuzco12 <oudar.pierre@gmail.com>
2015-05-16 17:44:29 +0200

    creation de la view index pour test

--- recette/views.py
+++ recette/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from .models import *
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
 # Create your views here.
+
+class IndexView(generic.ListView):
+    model = Recette
+    template_name = 'recette/index.html'
+    context_object_name = 'recette'
+
